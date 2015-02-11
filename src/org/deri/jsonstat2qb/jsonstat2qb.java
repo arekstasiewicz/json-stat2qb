e942d2a9328d7d4bb0185a7d4c653036f7c121e1
Arkadiusz Stasiewicz <arkadiusz.stasiewicz@deri.org>
2015-02-11 13:55:52 +0000

    arguments

--- src/org/deri/jsonstat2qb/jsonstat2qb.java
+++ src/org/deri/jsonstat2qb/jsonstat2qb.java
@@ -1,13 +1,18 @@
 package org.deri.jsonstat2qb;
 
+import java.io.IOException;
 import java.net.JarURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.jar.Manifest;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
 import arq.cmdline.ArgDecl;
 import arq.cmdline.CmdGeneral;
+
+import com.hp.hpl.jena.shared.NotFoundException;
+import com.hp.hpl.jena.sparql.util.Utils;
 
 public class jsonstat2qb extends CmdGeneral {
 	
@@ -30,11 +35,11 @@ public class jsonstat2qb extends CmdGeneral {
 		BUILD_DATE = date;
 	}
 
-	public static void main(String... args) {
+	public static void main(String[] args) {
 		new jsonstat2qb(args).mainRun();
 	}
 
-	private String datasetUrl;
+	private String datasetUrl = null;
 	private String encoding = null;
 	private boolean writeNTriples = false;
 		
@@ -44,23 +49,26 @@ public class jsonstat2qb extends CmdGeneral {
 
 	public jsonstat2qb(String[] args) {
 		super(args);
+
 		getUsage().startCategory("Options");
 		add(encodingArg,      "-e   --encoding", "Override source file encoding (e.g., utf-8 or latin-1)");
 		add(nTriplesArg,      "--ntriples", "Write N-Triples instead of Turtle");
 		getUsage().startCategory("Main arguments");
+
 		getUsage().addUsage("datasetUrl", "Link to the converted file.");
+
 		modVersion.addClass(jsonstat2qb.class);
 
 	}
 
 	@Override
 	protected String getSummary() {
-		// TODO Auto-generated method stub
-		return null;
+		return getCommandName() + " datasetUrl [options]";
 	}
 
 	@Override
 	protected void processModulesAndArgs() {
+
 		if (getPositional().isEmpty()) {
 			doHelp();
 		}
@@ -73,29 +81,40 @@ public class jsonstat2qb extends CmdGeneral {
 			writeNTriples = true;
 		}
 
-		if (hasArg(datasetUrl)) {
-			datasetUrl = getValue(datasetUrl);
-			if (datasetUrl == null || datasetUrl.length() < 1) {
-				cmdError("Value of datasetUrl must be valid URL");
-			}
+		datasetUrl = getPositionalArg(0);
+		if (datasetUrl == null || datasetUrl.length() < 1) {
+			cmdError("Value of datasetUrl must be valid URL");
 		}
+
 	}
 
 	@Override
 	protected void exec() {
-		// TODO Auto-generated method stub
-		
+		initLogging();
+		try {
+			if (encoding != null) {
+				System.out.println(encoding);
+			}
+		} catch (NotFoundException ex) {
+			cmdError("Not found: " + ex.getMessage());
+		}
 	}
 
 	@Override
-	protected String getCommandName() {
-		// TODO Auto-generated method stub
-		return null;
+    protected String getCommandName() {
+		return Utils.className(this);
 	}
-	
-//    public static void main(String[] args) {
-//    	System.out.println("Hello World!"); 
-//    	System.out.println(VERSION); 
-//    	System.out.println(BUILD_DATE); 
-//    }
+
+	private void initLogging() {
+		if (isQuiet()) {
+			Logger.getRootLogger().setLevel(Level.ERROR);
+		}
+		if (isVerbose()) {
+			Logger.getLogger("org.deri.jsonstat2qb").setLevel(Level.INFO);
+		}
+		if (isDebug()) {
+			Logger.getLogger("org.deri.jsonstat2qb").setLevel(Level.DEBUG);
+		}
+	}
+
 }
