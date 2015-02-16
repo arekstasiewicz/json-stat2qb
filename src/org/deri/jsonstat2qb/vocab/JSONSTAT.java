9433590800b4b6a987efe399fcfb4bb79de94ecb
Arkadiusz Stasiewicz <arkadiusz.stasiewicz@deri.org>
2015-02-16 16:26:26 +0000

    formatting

--- src/org/deri/jsonstat2qb/vocab/JSONSTAT.java
+++ src/org/deri/jsonstat2qb/vocab/JSONSTAT.java
@@ -10,14 +10,17 @@ public class JSONSTAT {
     public static String getURI() {
         return uri;
     }
-    protected static final String uri = "http://json-states.org/json-stat#";
+    
+    protected static final String uri = "http://json-state.org/json-stat#";
+
     private static Model m = ModelFactory.createDefaultModel();
+
     public static final Resource Dataset = m.createResource(uri + "Dataset");
     public static final Property dimensions = m.createProperty(uri + "dimensions");
     public static final Resource Dimension = m.createResource(uri + "Dimension");
     public static final Property indexs = m.createProperty(uri + "indexs");
     public static final Property labels = m.createProperty(uri + "labels");
-    public static final Resource KeyValue= m.createResource(uri + "KeyValue");
+    public static final Resource KeyValue = m.createResource(uri + "KeyValue");
     public static final Property key = m.createProperty(uri + "key");
     public static final Property value = m.createProperty(uri + "value");
 }
