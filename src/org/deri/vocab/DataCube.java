828b24450986861a744c7614c436b8c5a6d5d0e4
Arkadiusz Stasiewicz <arkadiusz.stasiewicz@deri.org>
2015-02-17 17:33:12 +0000

    DataCube vocab + restructured package

---
+++ src/org/deri/vocab/DataCube.java
@@ -0,0 +1,56 @@
+package org.deri.vocab;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.Resource;
+
+public class DataCube {
+
+	private static Model m = ModelFactory.createDefaultModel();
+
+    public static final String NS = "http://purl.org/linked-data/cube#";
+
+    public static String getURI() { return NS; }
+
+    public static final Resource NAMESPACE = m.createResource( NS );
+
+    public static final Property attribute = m.createProperty( NS + "attribute" );
+    public static final Property codeList = m.createProperty( NS + "codeList" );
+    public static final Property component = m.createProperty( NS + "component" );
+    public static final Property componentAttachment = m.createProperty( NS + "componentAttachment" );
+    public static final Property componentProperty = m.createProperty( NS + "componentProperty" );
+    public static final Property componentRequired = m.createProperty( NS + "componentRequired" );
+    public static final Property concept = m.createProperty( NS + "concept" );
+    public static final Property dataSet = m.createProperty( NS + "dataSet" );
+    public static final Property dimension = m.createProperty( NS + "dimension" );
+    public static final Property hierarchyRoot = m.createProperty( NS + "hierarchyRoot" );
+    public static final Property measure = m.createProperty( NS + "measure" );
+    public static final Property measureDimension = m.createProperty( NS + "measureDimension" );
+    public static final Property measureType = m.createProperty( NS + "measureType" );
+    public static final Property observation = m.createProperty( NS + "observation" );
+    public static final Property observationGroup = m.createProperty( NS + "observationGroup" );
+    public static final Property order = m.createProperty( NS + "order" );
+    public static final Property parentChildProperty = m.createProperty( NS + "parentChildProperty" );
+    public static final Property slice = m.createProperty( NS + "slice" );
+    public static final Property sliceKey = m.createProperty( NS + "sliceKey" );
+    public static final Property sliceStructure = m.createProperty( NS + "sliceStructure" );
+    public static final Property structure = m.createProperty( NS + "structure" );
+
+    public static final Resource Attachable = m.createResource( NS + "Attachable" );
+    public static final Resource AttributeProperty = m.createResource( NS + "AttributeProperty" );
+    public static final Resource CodedProperty = m.createResource( NS + "CodedProperty" );
+    public static final Resource ComponentProperty = m.createResource( NS + "ComponentProperty" );
+    public static final Resource ComponentSet = m.createResource( NS + "ComponentSet" );
+    public static final Resource ComponentSpecification = m.createResource( NS + "ComponentSpecification" );
+    public static final Resource DataSet = m.createResource( NS + "DataSet" );
+    public static final Resource DataStructureDefinition = m.createResource( NS + "DataStructureDefinition" );
+    public static final Resource DimensionProperty = m.createResource( NS + "DimensionProperty" );
+    public static final Resource HierarchicalCodeList = m.createResource( NS + "HierarchicalCodeList" );
+    public static final Resource MeasureProperty = m.createResource( NS + "MeasureProperty" );
+    public static final Resource Observation = m.createResource( NS + "Observation" );
+    public static final Resource ObservationGroup = m.createResource( NS + "ObservationGroup" );
+    public static final Resource Slice = m.createResource( NS + "Slice" );
+    public static final Resource SliceKey = m.createResource( NS + "SliceKey" );
+
+}
---
+++ src/org/deri/vocab/JSONSTAT.java
@@ -0,0 +1,28 @@
+package org.deri.vocab;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.Resource;
+
+public class JSONSTAT {
+
+    public static String getURI() {
+        return uri;
+    }
+
+    protected static final String uri = "http://json-stat.org/json-stat#";
+
+    private static Model m = ModelFactory.createDefaultModel();
+
+    public static final Resource Dataset = m.createResource(uri + "Dataset");
+    public static final Property dimensions = m.createProperty(uri + "dimensions");
+
+    public static final Resource Dimension = m.createResource(uri + "Dimension");
+    public static final Property indexs = m.createProperty(uri + "indexs");
+    public static final Property labels = m.createProperty(uri + "labels");
+
+    public static final Resource KeyValue = m.createResource(uri + "KeyValue");
+    public static final Property key = m.createProperty(uri + "key");
+    public static final Property value = m.createProperty(uri + "value");
+}
