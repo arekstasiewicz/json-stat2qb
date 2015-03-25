34a794ad684e044332b490a63b25fe4784c0abd8
Arkadiusz Stasiewicz <arkadiusz.stasiewicz@deri.org>
2015-03-25 17:25:54 +0000

    safename fix

--- src/org/deri/jsonstat2qb/jsonstat/Helpers.java
+++ src/org/deri/jsonstat2qb/jsonstat/Helpers.java
@@ -3,7 +3,7 @@ package org.deri.jsonstat2qb.jsonstat;
 public class Helpers {
 
 	public static String makeSafeName(String name) {
-		return name.toLowerCase().replace(" ", "-").replaceAll("[^A-Za-z0-9]-", "");
+		return name.toLowerCase().replaceAll("[^A-Za-z0-9- ]", "").replace("  ", " ").replace(" ", "-");
 	}
 
 }
